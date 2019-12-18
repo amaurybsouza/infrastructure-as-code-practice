fccd84820f1738c2d20a76129d96b47a13a78dca
amaurybsouza <amaurybsouza@gmail.com>
2019-12-18 07:51:37 -0300

    new file .tf for nginx

---
+++ terraform/codigos/terraform-nginx.tf
@@ -0,0 +1,17 @@
+# Configure the Docker provider
+provider "docker" {
+  }
+# Create a Apache Container
+resource "docker_container" "nginx" {
+    image = "nginx"
+    name = "enginecks"
+    ports {
+        internal = "80"
+        external = "80"
+    }
+}
+# # Create a image
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+}
+
