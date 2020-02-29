d9c28b908f076393d85b6bd0b266a1874f251329
amaurybsouza <amaurybsouza@gmail.com>
2020-02-29 13:23:33 -0300

    teste

--- terraform/projetos/aws-s3:launch-buckets/main.tf
+++ terraform/projetos/aws-s3:launch-buckets/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "mybucket-teste" {
+  bucket = "my-bucket-teste-aws"
+  acl = "private"
+
+  tags = {
+      Name = "My bucket-1.0"
+      Environment = "Dev"
+  }
+}
+
