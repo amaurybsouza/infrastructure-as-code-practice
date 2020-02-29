9f3cc98bb1a1ff9b26daa79b9fb7c2c327e7023f
root <amaurybsouza@gmail.com>
2020-02-29 12:07:33 -0300

    new files

---
+++ terraform/projetos/aws-ec2:launch-intances-1.0/instances.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_file = "/home/amaury/.aws/credentials"
+    profile = "boblee"
+}
+
+resource "aws_instance" "new-instance" {
+  ami = "ami-00a208c7cdba991ea"
+  instance_type = "t2.micro"
+}
